8be7ae83fe01d00ceba7a7ddbd18ac8d050eac79
Cowboy S <73200122+Potatzz@users.noreply.github.com>
2026-01-23 15:34:04 -0800

    Delete src/main directory

--- src/main/java/frc/robot/subsystems/auton/AutonConstants.java
+++
@@ -1,3 +0,0 @@
-package frc.robot.subsystems.auton;
-
-public record AutonConstants() {}
--- src/main/java/frc/robot/util/Constants.java
+++
@@ -1,8 +0,0 @@
-package frc.robot.util;
-
-public class Constants {
-	/** Use {@link frc.robot.Robot#drivebus} instead */
-	@SuppressWarnings("DeprecatedIsStillUsed")
-	@Deprecated
-	public static final String drivebus = "Drivebus";
-}
