0cb0b421767d2f0dbce725167b9bea0f70b165e1
Cowboy Stinson <cstinson29@priorypanther.com>
2026-02-09 19:31:15 -0800

    TRYING to throttle the speeds of the shooter
    yeetus deletus maple sim

--- src/main/java/frc/robot/util/ControlBoard.java
+++ src/main/java/frc/robot/util/ControlBoard.java
@@ -84,6 +84,14 @@ public class ControlBoard {
 			new StartEndCommand(() -> shooter.newShooterSpeed(7), () -> shooter.requestIdle())
 		);
 
+		// controller.rightTrigger.whileTrue(
+		// 	new StartEndCommand(() -> shooter.newShooterSpeed(controller.getRightTrigger()), () -> shooter.requestIdle())
+		// );
+
+		controller.rightTrigger.whileTrue(
+			new StartEndCommand(() -> System.out.println(controller.getRightTrigger()), () -> System.out.println("Stopped"))
+		);
+
 		
 	}
 
