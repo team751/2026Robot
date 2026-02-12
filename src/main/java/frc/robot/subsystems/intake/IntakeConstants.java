e0121c2eb4c33b852f5e9ed12bc9c77aaf42d0f8
lasersushi <lwalker30@priorypanther.com>
2026-02-11 18:41:19 -0800

    Adding neutral mode

--- src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems.intake;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import frc.lib.CTREConfig;
 import frc.robot.Robot;
@@ -38,6 +39,7 @@ public class IntakeConstants {
 	intakeConfig.TorqueCurrent.PeakReverseTorqueCurrent = -40;
 
 	intakeConfig.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive; // Positive intake
+	intakeConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
 }
 public static final CTREConfig<TalonFX, TalonFXConfiguration> extenderMotorConfig =
 	new CTREConfig<>(TalonFXConfiguration::new);
@@ -62,5 +64,6 @@ public static final CTREConfig<TalonFX, TalonFXConfiguration> extenderMotorConfi
 	extenderConfig.TorqueCurrent.PeakReverseTorqueCurrent = -40;
 
 	extenderConfig.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive; // Positive intake
+	extenderConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
 }
 }
