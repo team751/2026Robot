17037a500b17f8cdc66c67b4c6dd7e7457b409bf
Cowboy Stinson <cstinson29@priorypanther.com>
2026-02-11 20:41:57 -0800

    added the constants stuff into the subsystem file

--- src/main/java/frc/robot/subsystems/climber/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberSubsystem.java
@@ -1,6 +1,5 @@
 package frc.robot.subsystems.climber;
 
-import org.dyn4j.dynamics.Torque;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.DutyCycleOut;
@@ -15,15 +14,12 @@ import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.MotorAlignmentValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-//import frc.robot.subsystems.climber.ClimberConstants;
+import frc.robot.subsystems.climber.ClimberConstants;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class ClimberSubsystem extends SubsystemBase {
-private final TalonFX leftClimber = new TalonFX(10);
-private final TalonFX rightClimber = new TalonFX(11);
 
 private final PositionVoltage positionRequest = new PositionVoltage(0);
 
@@ -39,36 +35,29 @@ public static ClimberSubsystem getInstance() {
 }
 
 private ClimberSubsystem() {
-	TalonFXConfiguration config = new TalonFXConfiguration();
-	config.Slot0.kP = 1;
-	config.Slot0.kI = 0.0;
-	config.Slot0.kD = 0.3;
+	// TalonFXConfiguration config = new TalonFXConfiguration();
+	// config.Slot0.kP = 1;
+	// config.Slot0.kI = 0.0;
+	// config.Slot0.kD = 0.3;
 
-	config.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+	// config.MotorOutput.NeutralMode = NeutralModeValue.Brake;
 	
 
-	leftClimber.getConfigurator().apply(config);
-	rightClimber.getConfigurator().apply(config);
+	// leftClimber.getConfigurator().apply(config);
+	// rightClimber.getConfigurator().apply(config);
 
-	// Register so periodic() will be called by the CommandScheduler
-	CommandScheduler.getInstance().registerSubsystem(this);
 
-	// Set neutral mode
-	// dont invert
+	//CommandScheduler.getInstance().registerSubsystem(this);
 }
 
 @Override
 public void periodic() {
-	SmartDashboard.putNumber("Left Motor Pos", leftClimber.getPosition().getValueAsDouble());
-	SmartDashboard.putNumber("Right Motor Pos", rightClimber.getPosition().getValueAsDouble());
+	SmartDashboard.putNumber("Left Motor Pos", ClimberConstants.leftClimber.getPosition().getValueAsDouble());
+	SmartDashboard.putNumber("Right Motor Pos", ClimberConstants.rightClimber.getPosition().getValueAsDouble());
 
-	SmartDashboard.putNumber("SpinUntil/Target", m_spinTarget);
-	SmartDashboard.putBoolean("SpinUntil/Init",m_spinUntilTarget);
-	// If requested, run the spin-until-target logic non-blocking so callers don't have to loop.
 	if (m_spinUntilTarget) {
-		double pos = leftClimber.getPosition().getValueAsDouble();
 		SmartDashboard.putNumber("SpinUntil/Target", m_spinTarget);
-		if (pos >= m_spinTarget) {
+		if (ClimberConstants.leftClimber.getPosition().getValueAsDouble() >= m_spinTarget) {
 			stopMotors();
 			m_spinUntilTarget = false;
 			SmartDashboard.putBoolean("SpinUntil/Stopped", true);
@@ -79,32 +68,26 @@ public void periodic() {
 
 }
 
-// write a methods (think of a function, ask chatgpt if u dont get it) that:
-// moves the climber motors up to 180 degrees
-// moves the climber motors down to 180 degrees
-// stops the motors
 
 public void moveVerySlowly() {
-		leftClimber.setVoltage(0.5);
-		rightClimber.setVoltage(0.5);
+		ClimberConstants.leftClimber.setVoltage(0.5);
+		ClimberConstants.rightClimber.setVoltage(0.5);
 }
 
 public void motorSync() {
-	leftClimber.setControl(new DutyCycleOut(0.1));
-	rightClimber.setControl(new Follower(10, MotorAlignmentValue.Aligned));
+	ClimberConstants.leftClimber.setControl(new DutyCycleOut(0.1));
+	ClimberConstants.rightClimber.setControl(new Follower(10, MotorAlignmentValue.Aligned));
 }
 
 public void spinUntil10() {
-	// Start a non-blocking spin that will stop when the left encoder reaches 10 rotations.
 	m_spinTarget = 10.0;
 	m_spinUntilTarget = true;
 	if (m_spinUntilTarget) {
-		leftClimber.setControl(new DutyCycleOut(0.1));
-		rightClimber.setControl(new Follower(10, MotorAlignmentValue.Aligned));
+		ClimberConstants.leftClimber.setControl(new DutyCycleOut(0.1));
+		ClimberConstants.rightClimber.setControl(new Follower(10, MotorAlignmentValue.Aligned));
 	}
 }
 
-/** Cancel an ongoing spin-until operation (if any) and stop the motors. */
 public void cancelSpinUntil() {
 	m_spinUntilTarget = false;
 	stopMotors();
@@ -112,32 +95,32 @@ public void cancelSpinUntil() {
 
 public void moveUp180(){
 
-	double currentPosition = leftClimber.getPosition().getValueAsDouble();
+	double currentPosition = ClimberConstants.leftClimber.getPosition().getValueAsDouble();
 	double targetPosition = currentPosition + 1.0;
 
-	leftClimber.setControl(positionRequest.withPosition(targetPosition));
-	rightClimber.setControl(positionRequest.withPosition(targetPosition));
+	ClimberConstants.leftClimber.setControl(positionRequest.withPosition(targetPosition));
+	ClimberConstants.rightClimber.setControl(positionRequest.withPosition(targetPosition));
 
 }
 
 public void moveDown180(){
 
-	double currentPosition = leftClimber.getPosition().getValueAsDouble();
+	double currentPosition = ClimberConstants.leftClimber.getPosition().getValueAsDouble();
 	double targetPosition = currentPosition - 1.0;
 
-	leftClimber.setControl(positionRequest.withPosition(targetPosition));
-	rightClimber.setControl(positionRequest.withPosition(targetPosition));
+	ClimberConstants.leftClimber.setControl(positionRequest.withPosition(targetPosition));
+	ClimberConstants.rightClimber.setControl(positionRequest.withPosition(targetPosition));
 
 }
 
 public void stopMotors(){
-	leftClimber.stopMotor();
-	rightClimber.stopMotor();
+	ClimberConstants.leftClimber.stopMotor();
+	ClimberConstants.rightClimber.stopMotor();
 }
 
 public void zeroClimber(){
-	leftClimber.setPosition(0);
-	rightClimber.setPosition(0);
+	ClimberConstants.leftClimber.setPosition(0);
+	ClimberConstants.rightClimber.setPosition(0);
 }
 
 
