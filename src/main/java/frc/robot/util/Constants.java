958cc6c37a5ec25fc5b1bb09305746313652baa3
lasersushi <lwalker30@priorypanther.com>
2026-02-11 17:37:37 -0800

    Adding field constants

--- src/main/java/frc/robot/util/Constants.java
+++ src/main/java/frc/robot/util/Constants.java
@@ -1,8 +1,12 @@
 package frc.robot.util;
 
+import edu.wpi.first.hal.HAL;
+
 public class Constants {
 	/** Use {@link frc.robot.Robot#drivebus} instead */
 	@SuppressWarnings("DeprecatedIsStillUsed")
 	@Deprecated
 	public static final String drivebus = "Drivebus";
+
+	public static final boolean disableHAL = !HAL.initialize(500, 0);
 }
