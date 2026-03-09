cf6ba4d4c5f7bdb6bdb79bad2977b4aee6b30344
Cowboy Stinson <cstinson29@priorypanther.com>
2026-03-09 16:01:51 -0700

    limelight guide

--- src/main/java/frc/robot/subsystems/vision/LimelightConstants.java
+++ src/main/java/frc/robot/subsystems/vision/LimelightConstants.java
@@ -18,22 +18,25 @@
 
 // public class LimelightConstants {
 //   public static class LimelightFront {
-//     public static final String version = "3G";
+//     public static final String version = "3G"; // TODO: check the versions for both cameras
 //     public static final String streamIp = "http://10.7.51.71:5800";
 //     // For ShuffleBoard. Hopefully this is never used :D
 //     public static final String shuffleStreamIp = "mjpeg:http://10.7.51.71:5800";
 //     public static final String dashboardIp = "http://10.7.51.71:5801";
-//     public static final String name = "limelight-front";
+//     public static final String name = "limelight-front"; // TODO: confirm/change the names to make sense. do it on both the camera and in code
 //     public static final double pipelineLatencySeconds = 0.0;
 
-//     public static final Distance zOffset = Units.Meters.of(0.235); // Up/Down (+ up, - down)
+//     public static final Distance zOffset = Units.Meters.of(0.235); // Up/Down (+ up, - down) //TODO: change the offset from the CENTER of the robot for both cameras
 //     public static final Distance yOffset = Units.Meters.of(0.025); // Left/Right (+ left, - right)
 //     public static final Distance xOffset =
 //         Units.Meters.of(0.225); // Forward/Backward (- forward, + backward)
 
 //     public static final Rotation3d rotationOffset =
-//         new Rotation3d(0, 0, 0); // Left is - and right is + (degrees)
-//   }
+//         new Rotation3d(
+//              0,
+//              0,
+//              Units.Radians.convertFrom(0, Units.Degrees)); // Left is - and right is + (degrees) // TODO: also do the rotation offset. camera-relative.
+//   }          //TODO: ^^^ use that to convert from degrees to radians. radians are complicated and scary and degrees make more sense :)
 
 //   public static class LimelightBack {
 //     public static final String version = "2";
