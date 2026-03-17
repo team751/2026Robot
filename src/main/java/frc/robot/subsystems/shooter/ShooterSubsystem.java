9a1894e1e48fb67baee25ef511ee7fbf11164b5f
Cowboy Stinson <cstinson29@priorypanther.com>
2026-03-16 17:33:31 -0700

    shooter tuning works???????????
    we altered field constants hopefully its fine 😭

--- src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
@@ -98,8 +98,8 @@ public class ShooterSubsystem extends SubsystemBase {
     }
 
     double distanceCM = getRobotDistanceFromHub();
-    return (distanceCM - ShooterConstants.shooterDistanceCurveYIntercept)
-        / ShooterConstants.shooterDistanceCurveSlope;
+    return ((distanceCM - ShooterConstants.shooterDistanceCurveYIntercept)
+        / ShooterConstants.shooterDistanceCurveSlope)-2;
   }
 
   public void requestIdle() {
