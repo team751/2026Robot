34e16dea0f4e195018b6bfad27db66b1a7dd646d
lasersushi <lwalker30@priorypanther.com>
2026-07-05 11:36:08 +0200

    chore: Adding photonvision constants

---
+++ src/main/java/frc/robot/subsystems/vision/PhotonvisionConstants.java
@@ -0,0 +1,18 @@
+package frc.robot.subsystems.vision;
+
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+
+public class PhotonvisionConstants {
+    public static final String FRONT_CAMERA_NAME = "front";
+    public static final String SIDE_CAMERA_NAME = "side";
+
+    public static final Transform3d FRONT_CAMERA_OFFSET = new Transform3d(
+            new Translation3d(LimelightConstants.LimelightFront.xOffset,LimelightConstants.LimelightFront.yOffset, LimelightConstants.LimelightFront.zOffset), //x, y, and z in meters
+            LimelightConstants.LimelightFront.rotationOffset
+    );
+    public static final Transform3d SIDE_CAMERA_OFFSET = new Transform3d(
+            new Translation3d(LimelightConstants.LimelightSide.xOffset,LimelightConstants.LimelightSide.yOffset, LimelightConstants.LimelightSide.zOffset), //x, y, and z in meters
+            LimelightConstants.LimelightSide.rotationOffset
+    );
+}
