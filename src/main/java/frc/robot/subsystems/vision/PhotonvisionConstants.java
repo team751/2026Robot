3f42d724ac4790fa3eedf11dccbc654e8f66901c
lasersushi <lwalker30@priorypanther.com>
2026-07-05 11:43:40 +0200

    chore: Formatting

--- src/main/java/frc/robot/subsystems/vision/PhotonvisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/PhotonvisionConstants.java
@@ -4,15 +4,21 @@ import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
 
 public class PhotonVisionConstants {
-    public static final String FRONT_CAMERA_NAME = "front";
-    public static final String SIDE_CAMERA_NAME = "side";
+  public static final String FRONT_CAMERA_NAME = "front";
+  public static final String SIDE_CAMERA_NAME = "side";
 
-    public static final Transform3d FRONT_CAMERA_OFFSET = new Transform3d(
-            new Translation3d(LimelightConstants.LimelightFront.xOffset,LimelightConstants.LimelightFront.yOffset, LimelightConstants.LimelightFront.zOffset), //x, y, and z in meters
-            LimelightConstants.LimelightFront.rotationOffset
-    );
-    public static final Transform3d SIDE_CAMERA_OFFSET = new Transform3d(
-            new Translation3d(LimelightConstants.LimelightSide.xOffset,LimelightConstants.LimelightSide.yOffset, LimelightConstants.LimelightSide.zOffset), //x, y, and z in meters
-            LimelightConstants.LimelightSide.rotationOffset
-    );
+  public static final Transform3d FRONT_CAMERA_OFFSET =
+      new Transform3d(
+          new Translation3d(
+              LimelightConstants.LimelightFront.xOffset,
+              LimelightConstants.LimelightFront.yOffset,
+              LimelightConstants.LimelightFront.zOffset), // x, y, and z in meters
+          LimelightConstants.LimelightFront.rotationOffset);
+  public static final Transform3d SIDE_CAMERA_OFFSET =
+      new Transform3d(
+          new Translation3d(
+              LimelightConstants.LimelightSide.xOffset,
+              LimelightConstants.LimelightSide.yOffset,
+              LimelightConstants.LimelightSide.zOffset), // x, y, and z in meters
+          LimelightConstants.LimelightSide.rotationOffset);
 }
